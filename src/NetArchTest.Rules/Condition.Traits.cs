e62885d9de9777a31d42af9dafdb1c2e7f95b6c0
NeVeSpl <nevespl@gmail.com>
2023-11-19 19:26:45 +0100

    refactoring : extract Traits to separate files

---
+++ src/NetArchTest.Rules/Condition.Traits.cs
@@ -0,0 +1,248 @@
+﻿using NetArchTest.Functions;
+
+namespace NetArchTest.Rules
+{
+    public sealed partial class Condition
+    {
+        /// <summary>
+        /// Selects types that are marked as abstract.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeAbstract()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeAbstract(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not marked as abstract.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeAbstract()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeAbstract(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are classes.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeClasses()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeClass(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not classes.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeClasses()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeClass(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that have generic parameters.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeGeneric()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeGeneric(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that do not have generic parameters.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeGeneric()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeGeneric(x, false));
+            return CreateConditionList();
+        }
+
+
+        /// <summary>
+        /// Selects types that are interfaces.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeInterfaces()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeInterface(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not interfaces.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeInterfaces()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeInterface(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are static.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeStatic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeStatic(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not static.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeStatic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeStatic(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are nested.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeNested()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNested(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are nested and public.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeNestedPublic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNestedPublic(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are nested and private.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeNestedPrivate()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNestedPrivate(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not nested.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeNested()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNested(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not nested and public.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeNestedPublic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNestedPublic(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are not nested and private.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeNestedPrivate()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeNestedPrivate(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are have public scope.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BePublic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BePublic(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that do not have public scope.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBePublic()
+        {
+            AddFunctionCall(x => FunctionDelegates.BePublic(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types according that are marked as sealed.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeSealed()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeSealed(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types according that are not marked as sealed.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList NotBeSealed()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeSealed(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are immutable.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeImmutable()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeImmutable(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types that are mutable.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList BeMutable()
+        {
+            AddFunctionCall(x => FunctionDelegates.BeImmutable(x, false));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types according to whether they have nullable members.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList OnlyHaveNullableMembers()
+        {
+            AddFunctionCall(x => FunctionDelegates.HasNullableMembers(x, true));
+            return CreateConditionList();
+        }
+
+        /// <summary>
+        /// Selects types according to whether they have nullable members.
+        /// </summary>
+        /// <returns>An updated set of conditions that can be applied to a list of types.</returns>
+        public ConditionList HaveSomeNonNullableMembers()
+        {
+            AddFunctionCall(x => FunctionDelegates.HasNullableMembers(x, false));
+            return CreateConditionList();
+        }
+    }
+}
--- src/NetArchTest.Rules/Functions/FunctionDelegates.cs
+++ src/NetArchTest.Rules/Functions/FunctionDelegates.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Mono.Cecil;
-using Mono.Cecil.Rocks;
 using NetArchTest.Assemblies;
-using NetArchTest.Dependencies;
-using NetArchTest.RuleEngine;
 using NetArchTest.Rules;
-
 
 namespace NetArchTest.Functions
 {
@@ -65,6 +61,7 @@ namespace NetArchTest.Functions
                 return input.Where(c => !c.Definition.Name.EndsWith(end, comparer));
             }
         }
+
 
         /// <summary> Function for finding classes with a particular custom attribute. </summary>
         internal static IEnumerable<TypeSpec> HaveCustomAttribute(IEnumerable<TypeSpec> input, Type attribute, bool condition)
@@ -138,166 +135,7 @@ namespace NetArchTest.Functions
                 return input.Where(c => !found.Contains(c));
             }
         }
-
-        /// <summary> Function for finding abstract classes. </summary>
-        internal static IEnumerable<TypeSpec> BeAbstract(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsAbstract);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsAbstract);
-            }
-        }
-
-        /// <summary> Function for finding classes. </summary>
-        internal static IEnumerable<TypeSpec> BeClass(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsClass);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsClass);
-            }
-        }
-
-        /// <summary> Function for finding interfaces. </summary>
-        internal static IEnumerable<TypeSpec> BeInterface(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsInterface);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsInterface);
-            }
-        }
-
-        /// <summary> Function for finding static classes. </summary>
-        internal static IEnumerable<TypeSpec> BeStatic(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(ClassIsStatic);
-            }
-            else
-            {
-                return input.Where(c => !ClassIsStatic(c));
-            }
-
-            bool ClassIsStatic(TypeSpec c) => c.Definition.IsAbstract && c.Definition.IsSealed && !c.Definition.IsInterface && !c.Definition.GetConstructors().Any(m => m.IsPublic);
-        }
-
-        /// <summary> Function for finding types with generic parameters. </summary>
-        internal static IEnumerable<TypeSpec> BeGeneric(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.HasGenericParameters);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.HasGenericParameters);
-            }
-        }
-
-
-        /// <summary> Function for finding nested classes. </summary>
-        internal static IEnumerable<TypeSpec> BeNested(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsNested);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsNested);
-            }
-        }
-
-        /// <summary> Function for finding nested public classes. </summary>
-        internal static IEnumerable<TypeSpec> BeNestedPublic(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsNestedPublic);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsNestedPublic);
-            }
-        }
-
-        /// <summary> Function for finding nested private classes. </summary>
-        internal static IEnumerable<TypeSpec> BeNestedPrivate(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsNestedPrivate);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsNestedPrivate);
-            }
-        }
-
-
-        /// <summary> Function for finding public classes. </summary>
-        internal static IEnumerable<TypeSpec> BePublic(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsNested ? c.Definition.IsNestedPublic : c.Definition.IsPublic);
-            }
-            else
-            {
-                return input.Where(c => c.Definition.IsNested ? !c.Definition.IsNestedPublic : c.Definition.IsNotPublic);
-            }
-        }
-
-        /// <summary> Function for finding sealed classes. </summary>
-        internal static IEnumerable<TypeSpec> BeSealed(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsSealed);
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsSealed);
-            }
-        }
-
-        /// <summary> Function for finding immutable classes. </summary>
-        internal static IEnumerable<TypeSpec> BeImmutable(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.IsImmutable());
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.IsImmutable());
-            }
-        }
-
-        /// <summary> Function for finding nullable classes. </summary>
-        internal static IEnumerable<TypeSpec> HasNullableMembers(IEnumerable<TypeSpec> input, bool condition)
-        {
-            if (condition)
-            {
-                return input.Where(c => c.Definition.HasNullableMembers());
-            }
-            else
-            {
-                return input.Where(c => !c.Definition.HasNullableMembers());
-            }
-        }
+                     
 
         /// <summary> Function for finding types in a particular namespace. </summary>
         internal static IEnumerable<TypeSpec> ResideInNamespace(IEnumerable<TypeSpec> input, string name, bool condition)
